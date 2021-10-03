dde01945023acee05e1ca20b1cf00cb4b5ec4ba0
froddy <>
2021-10-03 16:29:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# demo linear search function
+# froddy
+# 26-09-2021
+
+# initialise and populate an array e.g. mylist = [1,2,3,4,5,6]
+# to search it: call function passing a list and the target value to find
+# linear_search(mylist, 4)
+# should return 4
+
+
+def linear_search(items, target):
+    #initialise variables
+    current_cell = 0  #starting position
+    found = False  # if we find it we can quit loop early
+
+    #repeat while current cell number less than list length and
+    #while target has not been found (still False)
+    while current_cell < len(items) and found == False:
+        if items[current_cell] == target:
+            found = True  # we found it! we can exit loop early :)
+        # otherwise proceed to next item (next cell)
+        # we need to increment the counter first though!
+        current_cell = current_cell + 1
+
+    #remember an array starts at 0, but it make more sense to people if we think of the first cell as 1, good job we have already added one then.
+    return current_cell
